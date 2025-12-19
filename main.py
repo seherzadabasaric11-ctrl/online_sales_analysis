fef2b27bac8ceb0c1e1a78517104958d0f0b72c1
Seherzada Basaric <seherzadabasaric11@gmail.com>
2025-12-19 20:31:13 +0100

    Modify products and remove invertory display from main

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from product import Product
 from product_menager import ProductMenager
 def main():
     menager=ProductMenager()
-    product1=Product(1,"Laptop","Electronics",1200,5)
-    product2=Product(2,"Phone","Electronics",800,10)
+    product1=Product(1,"Laptop Pro","Electronics",1200,3)
+    product2=Product(2,"Smartphone","Electronics",900,8)
     menager.add_product(product1)
     menager.add_product(product2)
     menager.list_products()
